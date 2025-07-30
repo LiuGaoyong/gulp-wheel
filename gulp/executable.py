67a6ce2f65af22cd25710c516c71f99565057115
LiuGaoyong <liugaoyong_88@163.com>
2025-07-30 09:58:27 +0800

    use `uv + scikit-build-core` instead of `pixi`

--- gulp/executable.py
+++ gulp/executable.py
@@ -1,17 +1,17 @@
 import os
 import subprocess
 import sys
-try:
-    from importlib import metadata
-except ImportError: # for Python<3.8
-    import importlib_metadata as metadata
+
 ROOT_DIR = os.path.dirname(__file__)
 
 
 def _program(name, args):
-    return subprocess.call([os.path.join(ROOT_DIR, name)] + args, close_fds=False)
+    return subprocess.call(
+        [os.path.join(ROOT_DIR, name)] + args,
+        close_fds=False,
+    )
 
 
 def gulp():
-    suffix = '.exe' if os.name == 'nt' else ''
-    raise SystemExit(_program('gulp' + suffix, sys.argv[1:]))
+    suffix = ".exe" if os.name == "nt" else ""
+    raise SystemExit(_program("gulp" + suffix, sys.argv[1:]))
